211d6505ec2cdafa81ab190c769dd91afc5a6108
Brennan Holten <bholten@protonmail.ch>
2024-02-15 03:22:32 -0600

    Update to current snatch snapshot branch

--- src/snatch_app_app.erl
+++ src/snatch_app_app.erl
@@ -10,9 +10,15 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
-    snatch_app_sup:start_link().
+    lager:info("Starting app"),
+    {ok, Queue1, Queue2} = get_config(),
+    snatch_app_sup:start_link(Queue1, Queue2).
 
 stop(_State) ->
     ok.
 
 %% internal functions
+get_config() ->
+    {ok, Queue1} = application:get_env(snatch_app, sqs_queue_1),
+    {ok, Queue2} = application:get_env(snatch_app, sqs_queue_2),
+    {ok, Queue1, Queue2}.
--- src/snatch_app_sup.erl
+++ src/snatch_app_sup.erl
@@ -1,13 +1,13 @@
 -module(snatch_app_sup).
 -behaviour(supervisor).
 
--export([start_link/0]).
+-export([start_link/2]).
 -export([init/1]).
 
-start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link(Queue1, Queue2) ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, [Queue1, Queue2]).
 
-init([]) ->
+init([Queue1, Queue2]) ->
     ChildSpec = [
         #{id => snatch_app_example_listener,
           start => {snatch_app_example_listener, start_link, []},
@@ -16,7 +16,13 @@ init([]) ->
           type => worker,
           modules => [snatch_app_example_listener]},
         #{id => claws_aws_sqs_1,
-          start => {claws_aws_sqs, start_link, [["000000000000/LocalSQSQueue1", "000000000000/LocalSQSQueue2"]]},
+          start => {claws_aws_sqs, start_link, [Queue1]},
+          restart => permanent,
+          shutdown => 5000,
+          type => worker,
+          modules => [claws_aws_sqs]},
+          #{id => claws_aws_sqs_2,
+          start => {claws_aws_sqs, start_link, [Queue2]},
           restart => permanent,
           shutdown => 5000,
           type => worker,
